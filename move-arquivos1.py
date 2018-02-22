b911bba080e520953736f3c7e0cf4f0aa9c035df
miti <leneanalista@gmail.com>
2018-02-22 15:47:41 -0300

    Melhorias no codigo

--- move-arquivos1.py
+++ move-arquivos1.py
@@ -4,53 +4,48 @@ import os
 from glob import glob
 import datetime
 from datetime import date
-from pprint import pprint
 
-def VerificaArquivo (arquivo, pasta):
-    local = os.chdir(pasta)
-    existe = os.path.isfile(arquivo)
+def VerificaArquivo (diretorio, arquivo):
+    print("*estou no verifica arquivo")
+    caminho = "%s%s" % (diretorio, arquivo)
+    existe = os.path.isfile(caminho)
     return existe
-
-def TrocaDiretorio (caminho):
-    diretorio = os.chdir(caminho)
-    return diretorio
 
 hoje = date.today()
 print("\nOi, hoje %s vou ajudá-lo a trabalhar com seus arquivos!" %hoje)
 print("Insira o caminho do diretório em que iremos trabalhar.")
-print("Como nos exemplos abaixo: \n--> /home/seu-usuario/Documentos *Documentos*\n--> /home/seu-usuario/.local/share/Trash/files *Lixeira*")
+print("Como nos exemplos abaixo: \n--> /home/seu-usuario/Documentos/ *Documentos*\n--> /home/seu-usuario/.local/share/Trash/files/ *Lixeira*")
 print("                         ")
+
 caminho = str(input("--> "))
-pasta = TrocaDiretorio(caminho)
-arquivos = os.listdir(pasta)
+origem = os.chdir(caminho)
+arquivos = os.listdir(origem)
 print("\n Estes são os arquivos existentes na pasta:")
 print(arquivos)
 
-#/home/miti/Documentos/pasta2
+#         /home/miti/Documentos/pasta2/
 cond = 0
 while cond <= 5:
         
     print("\nO que deseja fazer? ") 
     print("|| 1-Copiar || 2-Mover || 3-Deletar ||")
     opcao = int(input("--> "))
+# estou em pasta2
 
     if opcao == 1:
-        destino = str(input("Digite o destino: "))
-        
+        caminho = str(input("Digite o destino: "))
+
         for arquivo in arquivos:
-            VerificaArquivo(destino, arquivo) 
+            verifica = "%s%s" % (caminho, arquivo)
+            existe = os.path.isfile(verifica)
             
             if existe == False:
-                TrocaDiretorio(caminho)
-                shutil.copy(arquivo, destino)
+                shutil.copy(arquivo, caminho)
+                                   
             else:
                 print("o %s já existe no destino, não precisa mover" %arquivo)
                 print("                      ")       
-
-        print("                         ")
-        print("Os arquivos foram copiados")
-        destino = os.listdir(destino)
-        print(destino)    
+            
         break
 
     if opcao == 2:
@@ -59,7 +54,7 @@ while cond <= 5:
         for arquivo in arquivos:
             VerificaArquivo(destino, arquivo) 
 
-            if existe == False:
+            if existe == True:
                 TrocaDiretorio(caminho)
                 shutil.move(arquivo, destino)
             else:
